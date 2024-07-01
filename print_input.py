d2054e74e35820f1946e65899439755ad60fd2e1
renatoryu <renatoryuu@hotmail.com>
2024-07-01 16:14:25 -0300

    primeiro commit

---
+++ print_input.py
@@ -0,0 +1,6 @@
+nome = input("Digite seu nome: ")
+idade = input("Digite sua idade: ")
+
+print(f'Seu nome é {nome} e tem {idade} anos')
+print(f'Seu nome é {nome} e tem {idade} anos', end="... \n")
+print(nome, idade, sep=" | ")
